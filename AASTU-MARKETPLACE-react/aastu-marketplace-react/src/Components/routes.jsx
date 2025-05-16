8510b2fe393c9c726c990acde68df71b80b96da0
Eyuel M. Gedlie <eyuel.mazengia@aastustudent.edu.et>
2025-05-16 19:59:33 +0300

    Registration and login pages have been added

--- AASTU-MARKETPLACE-react/aastu-marketplace-react/src/Components/routes.jsx
+++ AASTU-MARKETPLACE-react/aastu-marketplace-react/src/Components/routes.jsx
@@ -1,32 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
-
 import Contactus from "../Pages/Contactus";
 import Home from "../Pages/Home";
-
-
 import Page404 from "../Pages/Page404";
 import AccountSettings from "../Pages/AccountSettings";
-
 import Cart from "../Pages/Cart";
 import Checkout from "../Pages/Checkout"; 
 import ProductDetails from "../Pages/ProductDetails";
 import { CartProvider } from "./CartContext";
-
-
-
 import Wishlist from "../Pages/Wishlist";
 import About from "../Pages/About";
 import Profile from "../Pages/Profile";
-
-
-
-
-
-
-
-
-
+import { Registration } from "../Pages/Registration";
+import LoginPage from "../Pages/login";
 
 const routes = createBrowserRouter([
   {
@@ -39,21 +25,14 @@ const routes = createBrowserRouter([
     children: [
       { path: "/contacus", element: <Contactus /> },
       { path: "/", element: <Home /> },
-
       { path: "contacus", element: <Contactus /> },
       { path: "accountsettings", element: <AccountSettings /> },
-      { path: "signup", element: <test /> },
-      // { path: "login", element: <Login /> },
-
+      { path: "signup", element: <Registration /> },
+      { path: "login", element: <LoginPage /> },
       { path: "*", element: <Page404 /> },
-
-      
       { path: "/wishlist", element: <Wishlist /> },
       { path: "/about", element: <About /> },
       { path: "/profile", element: <Profile /> },
-
-      
-
       {path:"cart", element: <Cart />},
       {path:"checkout", element: <Checkout />},
       {path:"productdetails", element: <ProductDetails />},
